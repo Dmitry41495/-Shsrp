51add99336372294a81bc5b4005bae2e9cae9f56
Dmitry <yudakoff.d@yandex.ru>
2022-07-16 00:16:40 +0300

    Рекурсия лекция

---
+++ Seminar/Seminar_5/Program.cs
@@ -0,0 +1,68 @@
+﻿// Задача 31: Задайте массив из 12 элементов, заполненный случайными числами из промежутка [-9, 9]. Найдите сумму отрицательных и положительных элементов массива.
+//Например, в массиве [3,9,-8,1,0,-7,2,-1,8,-3,-1,6] сумма положительных чисел равна 29, сумма отрицательных равна -20.
+
+
+
+System.Console.WriteLine("Введите длну массива");
+
+int arrayLength = 0;
+
+for (bool i = false; i == false;)
+{
+    while (!int.TryParse(Console.ReadLine(), out arrayLength))
+    {
+        System.Console.WriteLine("Неверный ввод, введите степень заново!");
+    }
+    if (arrayLength > 0)
+    {
+        i = true;
+    }
+    else
+    {
+        System.Console.WriteLine("Неверный ввод, введите степень заново!");
+    }
+}
+
+int[] createArray(int arrayLength, int minValue, int maxValue + 1)
+{
+    int[] array = new int[arrayLength];
+    for (int i = 0; i < arrayLength; i++)
+    {
+        array[i] = new Random().Next(minValue, maxValue);
+    }
+    return array;
+}
+
+int array[] = createArray(12, -9, 9);
+
+// int sumNumbersParametric(int[] array, bool findPositiveNumbers = true)
+// {
+//     int sum = 0;
+//     foreach ( int elem in array )
+//     {
+//         if(findPositiveNumbers)
+//         {
+//             sum += elem > 0 ? elem : 0;
+//         }
+//         else
+//         {
+//             sum += elem < 0 ? elem : 0;
+//         }
+//     }
+//     return sum;
+// }
+
+
+void printArray(int[] array)
+{
+    int size = array.Length;
+    for (int i = 0; i < size; i++)
+    {
+        System.Console.Write(string.Join("," array));
+    }
+}
+
+
+Console.WriteLine(printArray(createArray(arrayLength))));
+
+
---
+++ lecture/Example_013_Recursion_Algoritm/Program.cs
@@ -0,0 +1,148 @@
+﻿// Двумерный массив
+
+
+// string[,] table = new string[2, 5];
+
+// for(int rows = 0; rows < 2; rows++)
+// {
+//     for(int column = 0; column < 5; column++)
+//     {
+//         Console.WriteLine($"-{table[rows, column]}-");
+//     }
+// }
+
+// void  PrintArray(int[,] matrix)
+// {
+//     for(int rows = 0; rows < matrix.GetLength(0); rows++)
+//     {
+//         for(int column = 0; column < matrix.GetLength(1); column++)
+//         {
+//             Console.Write($"{matrix[rows, column]} ");
+//         }
+//         Console.WriteLine();
+//     }
+// }
+
+
+// void FillArray(int[,] matrix)
+// {
+//     int numbers;
+//     for(int rows = 0; rows < matrix.GetLength(0); rows++)
+//     {
+//         for(int column = 0; column < matrix.GetLength(1); column++)
+//         {
+//            while (!int.TryParse(Console.ReadLine(), out numbers))
+//             {
+//                  System.Console.WriteLine("Неверный ввод, введите число заново!");
+//             }
+//             matrix[rows, column] = numbers;
+//         }
+//     }
+// }
+
+// int[,] matrix = new int[3, 5];
+
+// PrintArray(matrix);
+
+// FillArray(matrix);
+
+// PrintArray(matrix);
+
+// void  PrintImage(int[,] image)
+// {
+//     for(int rows = 0; rows < image.GetLength(0); rows++)
+//     {
+//         for(int column = 0; column < image.GetLength(1); column++)
+//         {
+//             if(image[rows, column] == 0)
+//             {
+//                 Console.Write(" ");
+//             }
+//             else
+//             {
+//                 Console.Write("+");
+//             }
+            
+//         }
+//         Console.WriteLine();
+//     }
+// }
+
+// int[,] pic = new int[,]
+// {
+//  {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+//  {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+//  {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+//  {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+//  {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+//  {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+//  {0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+//  {0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+//  {0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+//  {0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0 },
+//  {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0 },
+//  {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0 },
+//  {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0 },
+//  {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0 },
+//  {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0 },
+//  {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0 },
+//  {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0 },
+//  {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0 },
+//  {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0 },
+//  {0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0 },
+//  {0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0 },
+//  {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0 },
+//  {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+// };
+
+// void FillImage(int row, int column)
+// {
+//     if (pic[row, column] == 0)
+//     {
+//         pic[row, column] = 1;
+//         FillImage(row - 1, column);
+//         FillImage(row, column - 1);
+//         FillImage(row + 1, column);
+//         FillImage(row, column + 1);
+//     }
+// }
+
+
+
+// PrintImage(pic);
+// FillImage(13, 13);
+// PrintImage(pic);
+
+
+
+// double Factorial(int n)
+// {
+//     if(n == 1) return 1;
+//     else return n * Factorial(n-1);
+    
+// }
+
+
+// for(int i = 1; i<40; i++)
+// {
+//     Console.WriteLine($"{i}! = {Factorial(i)}");
+// }
+
+// f(1) =1
+// f(2) = 1
+// f(n) = f(n-1) +f(n-2)
+
+int Fibonacci (int n)
+{
+    if (n == 1 || n == 2) return 1;
+    else return Fibonacci(n-1) + Fibonacci(n-2);
+}
+
+Console.WriteLine(Fibonacci(4));
+
+
+
+
+
+
+
