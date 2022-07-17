0c28fe2bb974cb53b65c374a2b817302a737a4ea
Dmitry <yudakoff.d@yandex.ru>
2022-07-17 17:08:20 +0300

    ДЗ 6 семинар

--- Seminar/Seminar_6/DZ_task_43/Program.cs
+++ Seminar/Seminar_6/DZ_task_43/Program.cs
@@ -1,5 +1,5 @@
-﻿// Напишите программу, которая найдёт точку пересечения двух прямых,
-//заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+﻿// // Напишите программу, которая найдёт точку пересечения двух прямых,
+// //заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
 
 System.Console.WriteLine("Введите параметр k1: ");
 int k1 = CheckingDataEntry();
@@ -10,9 +10,22 @@ int k2 = CheckingDataEntry();
 System.Console.WriteLine("Введите параметр b2: ");
 int b2 = CheckingDataEntry();
 
-System.Console.WriteLine("Координаты точки пересечения прямых с заданными параметрами равны:");
-System.Console.WriteLine($"x = {XCoordinateOfThePoint(k1, b1, k2, b2)}");
-System.Console.WriteLine($"y = {YCoordinateOfThePoint(k1, b1, k2, b2)}");
+if (k1 != k2)
+{
+    System.Console.WriteLine("Координаты точки пересечения прямых с заданными параметрами равны:");
+    System.Console.WriteLine($"x = {XCoordinateOfThePoint(k1, b1, k2, b2)}");
+    System.Console.WriteLine($"y = {YCoordinateOfThePoint(k1, b1, k2, b2)}");
+}
+else if (b1 == b2)
+{
+    System.Console.WriteLine("Заданные прямые совпадают (бесконечное множество точек пересечения)ю");
+}
+else
+{
+    System.Console.WriteLine("Заданные прямые паралельны и не пересекаются");
+}
+
+
 
 int CheckingDataEntry()
 {
@@ -27,14 +40,17 @@ int CheckingDataEntry()
     return outputData;
 }
 
-int XCoordinateOfThePoint(int k1, int b1, int k2, int b2)
+double XCoordinateOfThePoint(int k1, int b1, int k2, int b2)
 {
-    int x = (b2 - b1) / (k1 - k2);
+    double x = (double)(b2 - b1) / (k1 - k2);
     return x;
 }
 
-int YCoordinateOfThePoint(int k1, int b1, int k2, int b2)
+double YCoordinateOfThePoint(int k1, int b1, int k2, int b2)
 {
-    int y = k1 * (b2 - b1) / (k1 - k2) + b1;
+    double y = (double)k1 * (b2 - b1) / (k1 - k2) + b1;
     return y;
 }
+
+
+
